3e718ef3377fbaf3b98df3dac263d4df02d63670
unknown <javiervitalioperalta@gmail.com>
2025-10-28 18:31:36 -0400

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: false,
+
+  site: `https://test.vercel.app/`,
+
+  output: 'static',
+  adapter: vercel(),
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss(),
+
+    sitemap({
+      changefreq: 'weekly',
+      priority: 1,
+    }),]
+  },
+
+
 });
